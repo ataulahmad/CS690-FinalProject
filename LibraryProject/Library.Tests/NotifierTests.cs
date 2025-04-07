b119df285e7422a13fcac8d4dc24020fb2f202ce
Ataul Ahmad <ataulahmad@users.noreply.github.com>
2025-04-07 18:12:52 +0000

    sending notification done

---
+++ LibraryProject/Library.Tests/NotifierTests.cs
@@ -0,0 +1,40 @@
+namespace Library.Tests;
+
+using Library;
+
+public class NotifierTests {
+
+    LibraryInventory library;
+    Notifier notifier;
+
+    public NotifierTests() {
+        library = LibraryInventory.getInstance();
+        notifier = new Notifier();
+    }
+
+    [Fact]
+    public void SendNotificationTest() {
+        // given
+        File.Delete("book-data.txt");
+        File.Delete("customer-data.txt");
+
+        Customer customer = new Customer("name", "address", "email");
+        library.AddCustomer(customer);
+        Book book = new Book("title1", "genre", "isbn", "description", null, null);
+        library.AddBook(book);
+        library.CheckoutBookForCustomer(book, customer);
+        
+        book.dueDate = "01/01/2025";
+
+        using var sw = new StringWriter();
+        Console.SetOut(sw); // Redirect Console output
+
+        // when
+        notifier.CheckAndNotify();
+
+        // then
+        var expected = "Book title1 is over due. Sending email at email"+ Environment.NewLine;
+        Assert.Equal(expected, sw.ToString());
+    }
+
+}
--- LibraryProject/Library/Program.cs
+++ LibraryProject/Library/Program.cs
@@ -3,9 +3,16 @@
 class Program {
 
     static void Main(string[] args) {
-        ConsoleUI ui = new ConsoleUI();
-        ui.LoginMenu();
+        if (args.Length > 0) {
+            string command = args[0].ToLower();
+            if (command == "notify") {
+                Notifier notifier = new Notifier();
+                notifier.CheckAndNotify();
+            }
+        } else {
+            ConsoleUI ui = new ConsoleUI();
+            ui.LoginMenu();
+        }
     }
 
 }
-
